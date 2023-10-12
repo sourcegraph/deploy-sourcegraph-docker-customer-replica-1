20b3321725588579358443e9ad604c1062bbfbdb
Erik Seliger <erikseliger@me.com>
2023-10-12 15:14:32 +0200

    Remove GitHub proxy service (#49)
    See sourcegraph/sourcegraph#55290, we're removing this service in favor of deployment simplicity with a redis-based equivalent.

--- pure-docker/deploy-repo-updater.sh
+++ pure-docker/deploy-repo-updater.sh
@@ -21,7 +21,6 @@ docker run --detach \
     -e GOMAXPROCS=1 \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
     -e 'OTEL_EXPORTER_OTLP_ENDPOINT=http://otel-collector:4317' \
-    -e GITHUB_BASE_URL=http://github-proxy:3180 \
     -v $VOLUME:/mnt/cache \
     index.docker.io/sourcegraph/repo-updater:4.4.0@sha256:e4b71290ec21a6471eb4b9e1efb169af96212b7dfa72d9fd22ea722a49b3c707
 
